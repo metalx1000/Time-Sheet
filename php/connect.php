6b7adf9feed21b3a98818fed21250cf9172de2c3
metalx1000 <metalx1000@users.noreply.github.com>
2014-05-13 21:47:22 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost";
-$db_user = "metalx_work";
-$db_pass = "_bon2Pu*D!oC";
-$db_name = "metalx_work";
+$db_user = "user";
+$db_pass = "password";
+$db_name = "user";
 
 @mysql_connect("$db_host","$db_user","$db_pass") or die ("Could Not Connect");
 @mysql_select_db("$db_name") or die ("Could not find DB");
